c150086240eaaf22afcd8407ba0161a503721d9d
farzad <66000207+farzad-or@users.noreply.github.com>
2020-08-24 10:59:29 +0430

    edit

---
+++ tools/intialization.js
@@ -0,0 +1,29 @@
+// const User = require("../models/user");
+
+// const initialization = async function()  {
+//     try {
+//         const EXIST_ADMIN = await User.findOne({role: 'admin'});
+//         if (EXIST_ADMIN) {
+//             return console.log('Admin already created');
+//         };
+        
+//         const ADMIN = new User({
+
+//             userName:  "farzad",
+//             firstName: "farzad",
+//             lastName:"ordikhani",
+//             password:"66009096",
+//             email:"farzad.ordikhani@gmail.com",
+//             phones:"09384556161",
+//             gender:"male",
+//             role: 'admin'
+         
+//         });
+
+//         await ADMIN.save();
+
+//         console.log('Admin created');
+//     } catch (err) {
+//         console.log('Error in initialization function', err);
+//     };
+// };

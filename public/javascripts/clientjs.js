6ba60ef6db4bc457c9e988e582530c8f6cddfb8d
farzad <66000207+farzad-or@users.noreply.github.com>
2020-08-25 19:21:06 +0430

    avatar and profile edit

--- public/javascripts/clientjs.js
+++ public/javascripts/clientjs.js
@@ -30,20 +30,32 @@ $("#btn-signup").click(function () {
 })
 
 
+////////////////////////////////////////////////////////////////
+//            FUNC TO GET FORM DATA AS A OBJECT              //
+//////////////////////////////////////////////////////////////
+
+function getFormData(formId) {
+    let finalObject = {}
+    let formDataArr = $(`#${formId}`).serializeArray();
+
+    formDataArr.map(function (e) {
+
+        finalObject[e['name']] = e['value'];
+    })
+    return finalObject;
+}
+
 ///////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////// FUNC TO SIGNIN   ////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////////
 
 
 $("#btn-signIn").click(function () {
-    console.log(1000);
     let user = {
         'userName': $('#signin-username').val(),
         'password': $('#signin-password').val()
     }
-    console.log(1001);
-    console.log(user);
-
+ 
     $.ajax({
         type: "POST",
         url: "/api/signIn",
@@ -55,3 +67,80 @@ $("#btn-signIn").click(function () {
     });
 })
 
+
+////////////////////////////////////////////////////////////////
+//                      FUNC TO SIGNIN                       //
+//////////////////////////////////////////////////////////////
+
+$('#signinForm').submit(function (e) {
+    e.preventDefault();
+
+    $.ajax({
+        type: "POST",
+        url: "/api/signIn",
+        data: getFormData('signinForm'),
+        dataType: 'json',
+        success: function (res) {
+            if (res.state === 'true') {
+                console.log("sasas");
+                window.location = '/api/user/dashboard'
+            } else {
+                notification(res.message, res.state);
+            }
+        }
+    });
+})
+
+
+
+
+////////////////////////////////////////////////////////////////
+//               FUNC TO SAVE EDIT PROFILE INFO              //
+//////////////////////////////////////////////////////////////
+
+$('#saveEdit').click(function () {
+    console.log(111);
+console.log(getFormData('editInfoForm'));
+    // $.ajax({
+    //     type: "PUT",
+    //     url: "/api/user/update",
+    //     data: getFormData('editInfoForm'),
+    //     dataType: "json",
+    //     success: function (res) {
+         
+    // });
+})
+
+
+
+////////////////////////////////////////////////////////////////
+//                 FUNC TO SAVE PROFILE AVATAR               //
+//////////////////////////////////////////////////////////////
+
+$('#saveAvatar').click(function () {
+
+    if (!$('#field-file').val()) {
+        // notification("یک تصویر انتخاب کنید", "alert-warning");
+    } else {
+        let formData = new FormData();
+        let files = $('#field-file')[0].files[0];
+        formData.append('avatar', files);
+console.log(formData);
+        $.ajax({
+            url: '/api/user/uploadAvatar',
+            type: 'POST',
+            data: formData,
+            contentType: false,
+            processData: false,
+            success: function (res) {
+
+                // notification(res.message, res.color);
+                if (res.avatar) {
+                    // $('#avatarImg img.avatar-130').attr('src', `/images/avatar/${res.avatar}`)
+                    $('#avatarImage').attr('src', `/images/avatar/${res.avatar}`)
+                    // avatarCancel();
+                }
+            }
+        });
+    }
+});
